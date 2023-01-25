51536264e32a9ff259771b2616f926566a2ed23b
robwatsongtr <rwatso@gmail.com>
2023-01-25 13:23:09 -0800

    Revert "updated socials on header"
    This reverts commit 8f822c1690924ed37d4f08d276626d3db59bf8b3.

--- src/components/footer/FooterThree/index.js
+++ src/components/footer/FooterThree/index.js
@@ -7,7 +7,7 @@ const FooterThree = ({position}) => {
             <Container fluid>
                 <Row className={'align-items-center'}>
                     <Col className={'footer-copyright text-center'}>
-                        <p>&copy; {new Date().getFullYear()} ROB WATSON</p>
+                        <p>&copy; {new Date().getFullYear()} MINIMAU. ALL RIGHT RESERVED</p>
                     </Col>
                 </Row>
             </Container>
--- src/pages/home/HomePersonalPortfolio.js
+++ src/pages/home/HomePersonalPortfolio.js
@@ -5,6 +5,7 @@ import Contact from "../../container/contact";
 import SideHeader from "../../components/SideHeader";
 import Header from "../../components/header/HeaderOne";
 import Footer from "../../components/footer/FooterThree";
+import PersonalPortfolioTeam from "../../container/team";
 import PersonalPortfolioService from "../../container/service";
 import PersonalPortfolio from "../../container/portfolio/PersonalPortfolio";
 import BannerPersonalPortfolio from "../../components/HeroBanner/PersonalPortfolio";
@@ -17,6 +18,7 @@ const HomePersonalPortfolio = () => {
             <BannerPersonalPortfolio/>
             <PersonalPortfolio/>
             <PersonalPortfolioService/>
+            <PersonalPortfolioTeam/>
             <Contact/>
             <Footer position={'static'}/>
         </div>
