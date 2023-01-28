23e92a52b3b77f1e7201b7d4f1b281ce222f9902
robwatsongtr <rwatso@gmail.com>
2023-01-28 15:38:12 -0800

    changed title to software developer, added link to resume

--- src/components/HeroBanner/PersonalPortfolio/index.js
+++ src/components/HeroBanner/PersonalPortfolio/index.js
@@ -10,7 +10,14 @@ const BannerPersonalPortfolio = () => {
                 <h2>Hello , I’m</h2>
                 <h1>{personalInfo.name}</h1>
                 <h2>{personalInfo.designation}</h2>
-                <a href={`${process.env.PUBLIC_URL + personalInfo.resumeURL}`}>Download Resume</a>
+                <h3>
+                    <a 
+                        target="_blank" rel="noopener noreferrer" 
+                        href={`${process.env.PUBLIC_URL + personalInfo.resumeURL}`}
+                    >
+                        Download Resume
+                    </a>
+                </h3>
             </div>
         </HeroBannerContainer>
     );
